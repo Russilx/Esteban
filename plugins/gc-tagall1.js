7c3c05ffd9c9d240eb799d5af88eafd657a31c6c
KillzN <scorpionsheep203@gmail.com>
2024-09-07 14:36:21 -0400

    KILL - BOT

---
+++ plugins/gc-tagall1.js
@@ -0,0 +1,28 @@
+
+
+const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, command, usedPrefix}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.gc_tagall
+
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+  if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn);
+    throw false;
+  }
+  const pesan = args.join` `;
+  const oi = `*𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙎:* ${pesan}`;
+  let teks = `*> Yᴏᴜʀ Pᴏᴛᴇɴᴛɪᴀʟ Is Iɴғɪɴɪᴛᴇ, Dᴀʀᴇ Tᴏ Exᴘʟᴏʀᴇ Iᴛ 💋*\n\n ${oi}\n\n➥ _*@ineffable.mvrco:*_\n`;
+  for (const mem of participants) {
+    teks += `💗 ➸ @${mem.id.split('@')[0]}\n`;
+  }
+  teks += `└ *KɪʟʟBᴏᴛ ⇝@ineffable.mvrco*`;
+  conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
+};
+handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
+handler.tags = ['group'];
+handler.command = /^(tagall1|invocar1|invocacion1|todos1|invocación1)$/i;
+handler.admin = true;
+handler.group = true;
+export default handler;
