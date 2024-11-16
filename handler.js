93dbfc86d4ff06bcec9dc3c5cf4a3ee2ae72c988
KillzN <scorpionsheep203@gmail.com>
2024-11-16 00:35:36 -0300

    KILL - BOT

--- handler.js
+++ handler.js
@@ -1714,7 +1714,7 @@ export async function callUpdate(callUpdate) {
         const callmsg = await mconn.conn.reply(nk.from, `Hola *@${nk.from.split('@')[0]}*, las ${nk.isVideo ? 'videollamadas' : 'llamadas'} no están permitidas, serás bloqueado.\n-\nSi accidentalmente llamaste póngase en contacto con mi creador para que te desbloquee!`, false, { mentions: [nk.from] });
         // let data = global.owner.filter(([id, isCreator]) => id && isCreator)
         // await this.sendContact(nk.from, data.map(([id, name]) => [id, name]), false, { quoted: callmsg })
-        const vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;𝐁𝐫𝐮𝐧𝐨 𝐒𝐨𝐛𝐫𝐢𝐧𝐨 👑;;;\nFN:𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑\nORG:𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑\nTITLE:\nitem1.TEL;waid=5219992095479:+521 999 209 5479\nitem1.X-ABLabel:𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑\nX-WA-BIZ-DESCRIPTION:[❗] ᴄᴏɴᴛᴀᴄᴛᴀ ᴀ ᴇsᴛᴇ ɴᴜᴍ ᴘᴀʀᴀ ᴄᴏsᴀs ɪᴍᴘᴏʀᴛᴀɴᴛᴇs.\nX-WA-BIZ-NAME:𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑\nEND:VCARD`;
+        const vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;𝐁𝐫𝐮𝐧𝐨 𝐒𝐨𝐛𝐫𝐢𝐧𝐨 👑;;;\nFN:𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑\nORG:𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑\nTITLE:\nitem1.TEL;waid=56983073328:+569 983 073 328\nitem1.X-ABLabel:𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑\nX-WA-BIZ-DESCRIPTION:[❗] ᴄᴏɴᴛᴀᴄᴛᴀ ᴀ ᴇsᴛᴇ ɴᴜᴍ ᴘᴀʀᴀ ᴄᴏsᴀs ɪᴍᴘᴏʀᴛᴀɴᴛᴇs.\nX-WA-BIZ-NAME:𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑\nEND:VCARD`;
         await mconn.conn.sendMessage(nk.from, { contacts: { displayName: '𝗞𝗶𝗹𝗹𝘇𝗡 𝗦𝘁𝗼𝗿𝗲 👑', contacts: [{ vcard }] } }, { quoted: callmsg });
         await mconn.conn.updateBlockStatus(nk.from, 'block');
       }
