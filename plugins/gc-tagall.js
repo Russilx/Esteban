b95a2bd74c980cd27b970552ddbaaf6510868bd6
Russilx <sanmartinesteban123@gmail.com>
2024-12-12 02:40:09 -0300

    ZURDO

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -13,16 +13,16 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼𝙎:* ${pesan}`;
-  let teks = `*ZURDO SE LA COME*\n\n ${oi}\n\n➥ _*@tebi_.01:*_\n`;
+  let teks = `*EL ZURDO Y LA AGUS SON RE PUTAS*\n\n ${oi}\n\n➥ _*@tebi_.01:*_\n`;
   for (const mem of participants) {
     teks += `🥷🏼 ⇝ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `└ *KɪʟʟBᴏᴛ ⇝@tebi_.01*`;
+  teks += `└ *Bot EEA ⇝@tebi_.01*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
 handler.tags = ['group'];
-handler.command = /^(tagall|invocar|invocacion|todos|invocación|putos|idiotas)$/i;
+handler.command = /^(todos)$/i;
 handler.admin = true;
 handler.group = true;
 export default handler;
